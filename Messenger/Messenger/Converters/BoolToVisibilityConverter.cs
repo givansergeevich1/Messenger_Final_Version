74196949d68884a408534dcc2ea7b3c0cf4a2922
Ivan <g.ivan.sergeevich1@gamil.com>
2025-12-23 23:27:37 +1000

    Созданы конвертеры для форматирования дат и времени в пользовательском интерфейсе. Добавлены конвертеры для отображения относительного времени.

---
+++ Messenger/Messenger/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Messenger.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                if (parameter is string param && param == "invert")
+                {
+                    return boolValue ? Visibility.Collapsed : Visibility.Visible;
+                }
+
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Visible;
+            }
+
+            return false;
+        }
+    }
+
+    public class BoolToInverseVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Messenger/Messenger/Converters/DateTimeConverter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Messenger.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                var now = DateTime.Now;
+                var time = dateTime.ToLocalTime();
+
+                if (time.Date == now.Date)
+                {
+                    // Сегодня - показываем время
+                    return time.ToString("HH:mm");
+                }
+                else if (time.Date == now.Date.AddDays(-1))
+                {
+                    // Вчера
+                    return "Вчера " + time.ToString("HH:mm");
+                }
+                else if (time.Year == now.Year)
+                {
+                    // В этом году
+                    return time.ToString("dd MMM HH:mm");
+                }
+                else
+                {
+                    // Более года назад
+                    return time.ToString("dd.MM.yyyy HH:mm");
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TimeOnlyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToLocalTime().ToString("HH:mm");
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DateOnlyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                var now = DateTime.Now;
+                var time = dateTime.ToLocalTime();
+
+                if (time.Date == now.Date)
+                {
+                    return "Сегодня";
+                }
+                else if (time.Date == now.Date.AddDays(-1))
+                {
+                    return "Вчера";
+                }
+                else if (time.Year == now.Year)
+                {
+                    return time.ToString("dd MMM");
+                }
+                else
+                {
+                    return time.ToString("dd.MM.yyyy");
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

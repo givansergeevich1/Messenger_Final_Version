c86fce13a0a74f8d8e67d5a9a3d1c6d9634cf810
Ivan <g.ivan.sergeevich1@gamil.com>
2025-12-23 23:38:24 +1000

    Создана базовая реализация FirebaseAuthService с полями и конструктором. Инициализированы компоненты Firebase для работы с аутентификацией.

---
+++ Messenger/Messenger/Services/FirebaseAuthService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Messenger.Models;
+using Messenger.Services.Interfaces;
+using Messenger.Utils;
+
+namespace Messenger.Services
+{
+    public class FirebaseAuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+        private User? _currentUser;
+
+        public User? CurrentUser => _currentUser;
+        public event EventHandler<AuthStateChangedEventArgs>? AuthStateChanged;
+
+        public FirebaseAuthService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://identitytoolkit.googleapis.com/v1/");
+        }
+
+        protected virtual void OnAuthStateChanged(User? user)
+        {
+            AuthStateChanged?.Invoke(this, new AuthStateChangedEventArgs(user));
+        }
+
+        // Методы будут реализованы в следующих коммитах
+        public Task<User?> LoginAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> RegisterAsync(string email, string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ResetPasswordAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAuthenticatedAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetCurrentUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateUserProfileAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

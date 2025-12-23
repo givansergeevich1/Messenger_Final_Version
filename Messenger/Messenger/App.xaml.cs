b22dfcff33d6f8eee4a5a3c7255d4a39e16b736b
Ivan <g.ivan.sergeevich1@gamil.com>
2025-12-23 23:16:21 +1000

    Расширен класс App для поддержки Dependency Injection. Добавлена инициализация сервисов и навигации.

--- Messenger/Messenger/App.xaml.cs
+++ Messenger/Messenger/App.xaml.cs
@@ -1,8 +1,34 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Messenger.ViewModels;
+using Messenger.Services;
 
 namespace Messenger
 {
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрация сервисов будет добавлена позже
+            services.AddSingleton<MainViewModel>();
+
+            // Регистрация главного окна
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
     }
 }
